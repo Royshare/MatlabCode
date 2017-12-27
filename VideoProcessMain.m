8ed42de9cde34fbfc4a5089250c0cede52be9dfa
lr546 <lr546@10.128.92.171>
2017-12-27 14:50:34 -0600

    First version of VideoProcessMain - main code for get and crop image.

--- VideoProcessMain.m
+++ VideoProcessMain.m
@@ -9,27 +9,16 @@ close all
 VideoToProcess = VideoReader('MVI_0699.MOV');   
 FirstFrameIndex = 197;
 LastFrameIndex = 780;
-InletRadius= 5/32;   % unit: inch
+InletRadius= 5/32*2.54;   % unit: cm
 CroppedImageXPosition = 450;  
 CroppedImageYPosition = 98; 
 CroppedImageWidth = 1160;    
 CroppedImageHeight = 932;    
-MedianFilterThreshold = [3 3];  
-CannyVectorThreshold = [0.4 0.5]; 
-CannyVectorSigma = sqrt(2);     
-denom = 3;    %the parameter determines the threshold with which the code will remove unwanted points so as to keep points on edge only.
-
-IntensityDirectory = 'C:\Users\Rui\Desktop\phi22\Intensity Data';
-GreyImageDirectory = 'C:\Users\Rui\Desktop\phi22\Grey Image';
-GreyImageWithEdgeDirectory = 'C:\Users\Rui\Desktop\phi22\Grey Image with Edge';
-ColorImageDirectory = 'C:\Users\Rui\Desktop\phi22\Color Image';
+DataDirectory = 'C:\Users\Rui\Desktop\phi31';
+GrayImageDirectory = fullfile(DataDirectory,'Gray Image');
+GrayImageWithEdgeDirectory = fullfile(DataDirectory,'Gray Image with Edge');
+ColorImageDirectory = fullfile(DataDirectory,'Color Image');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-VideoHeight = VideoToProcess.Height; 
-VideoWidth = VideoToProcess.Width;
-
-FrameTotalNumber = LastFrameIndex-FirstFrameIndex+1;
-
 %SavedImageWidth = CroppedImageWidth/100;
 %SavedImageHeight = CroppedImageHeight/100;
 CropVector = [CroppedImageXPosition,CroppedImageYPosition,...
@@ -37,8 +26,31 @@ CropVector = [CroppedImageXPosition,CroppedImageYPosition,...
 %SaveVector = [0,0,SavedImageWidth,SavedImageHeight];
 
 % find ratio between real and image dimension. 
-[InletXPosition,InletYPostion,InletImageDiameter] = ...
+[InletRowPosition,InletColumnPostion,InletImageDiameter] = ...
                     findInletCenter(VideoToProcess,FirstFrameIndex,...
-                    CropVector,GreyImageWithEdgeDirectory,ColorImageDirectory);
+                 CropVector,GrayImageWithEdgeDirectory,ColorImageDirectory);
+Ratio = InletImageDiameter/InletRadius;     % unit: pixel/cm
+% crop and save every frame
+imageProcessIndex=1;
+for indexToProcessFrame = FirstFrameIndex:1:LastFrameIndex
+    [imageGrayCrop,imageColorCrop] = cropImage(VideoToProcess,...
+                              indexToProcessFrame,DataDirectory,CropVector);
+    imageName = sprintf(num2str(imageProcessIndex),'.png');
+    imshow(imageGrayCrop);
+    imageGrayCropDirectory = fullfile(GrayImageDirectory,imageName);
+    print('-dpng',imageGrayCropDirectory);
+    imageColorCropDirectory = fullfile(ColorImageDirectory,imageName);
+    print('-dpng',imageColorCropDirectory);
+    imageProcessIndex = imageProcessIndex+1;
+end
 
-% process every frame
+% save data useful for following process.
+
+output1 = ['Ratio(pixel/cm)','InletXPosition','InletYPosition',...
+                     'InletImageDiameter','starting frame','ending frame'];
+output2 = [Ratio,InletRowPosition,InletColumnPostion,InletImageDiameter,...
+                                            FirstFrameIndex,LastFrameIndex];
+xlsDirectory = fullfile(DataDirectory,'data.xls');
+xlswrite(xlsDirectory,output1,'A1');
+xlswrite(outputLoc,output2,'A2');
+
